f5cf5e6ea16a93dfdbec3f6f8a47501ea3704b41
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2022-09-25 20:31:53 -0600

    Update calculadora.cpp

--- calculadora.cpp
+++ calculadora.cpp
@@ -3,23 +3,23 @@
 // Suma dos numeros -----------------
 double suma(double x, double y)
 {
-    
+    return 0;
 }
 
 // Resta dos numeros -----------------
 double resta(double x, double y)
 {
-   
+   return 0;
 }
 
 // Multiplica dos numeros -------------
 double multiplica(double x, double y)
 {
-    
+    return 0;
 }
 
 // Divide dos numeros -----------------
 double divide(double x, double y)
 {
-    
+    return 0;
 }
